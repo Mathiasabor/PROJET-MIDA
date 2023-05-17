fe3a94c7c30cf969b42604ee82810cf308b774da
Mathiasabor <103271868+Mathiasabor@users.noreply.github.com>
2023-05-17 18:52:28 +0000

    mysql database making

--- bibliouut/settings.py
+++ bibliouut/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import MySQLdb
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,9 +97,20 @@ WSGI_APPLICATION = 'bibliouut.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+      #  'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': '4aLq7HlFQcH0dU387bU3',
+        'HOST': 'containers-us-west-58.railway.app',
+        'PORT': '6857',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",},
     }
 }
 
